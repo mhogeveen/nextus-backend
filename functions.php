49d8f1f66c198fe7a5cfa7447620393b65872f8f
Maarten Hogeveen <socialbrothers@maartens-air.lan>
2021-11-06 20:59:33 +0100

    Glob import theme files

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php defined('ABSPATH') || exit('Forbidden');
 
-require_once(get_template_directory() . '/library/theme-init.php');
 function nextus_setup()
 {
+  foreach (glob(get_template_directory() . '/library/theme/*.php') as $filename) {
+    require_once $filename;
+  }
 }
 add_action('after_setup_theme', 'nextus_setup');

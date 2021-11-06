b80c23d4fc90ad7d40b91cd4b5a030f4d7fa4184
Maarten Hogeveen <socialbrothers@maartens-air.lan>
2021-11-06 20:59:54 +0100

    Require composer generated autoload file

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 function nextus_setup()
 {
+  require_once get_template_directory() .  '/vendor/autoload.php';
+
   foreach (glob(get_template_directory() . '/library/theme/*.php') as $filename) {
     require_once $filename;
   }

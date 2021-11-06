38f98b91e707915c4f0f6b1c44202588fc7a770a
Maarten Hogeveen <socialbrothers@maartens-air.lan>
2021-11-06 20:59:04 +0100

    Wrap contents of functions.php in after_setup_theme hook

--- functions.php
+++ functions.php
@@ -1,3 +1,7 @@
 <?php defined('ABSPATH') || exit('Forbidden');
 
 require_once(get_template_directory() . '/library/theme-init.php');
+function nextus_setup()
+{
+}
+add_action('after_setup_theme', 'nextus_setup');
